742db88bb51d0c08e496e22429260c530815e519
DJ Wilkins <djwilkins5@gmail.com>
2018-09-23 21:03:31 -0400

    Added some code comments and removed course steps comment block

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Declare initial variables including key DOM element sections
 const myDeckElement = document.querySelector('.deck');
 const newGameButton = document.querySelector('.restart');
 const endGameScreen = document.querySelector('.modal')
@@ -8,6 +9,7 @@ let cardsFlipped = 0, cardOne, cardTwo, movesTaken = 0;
 let gameTime, timerRefresh;
 let howManyStars = 3;
 
+// Do initial board shuffle and start initial game timer.
 shuffleBoard();
 gameTimer();
 
@@ -262,18 +264,3 @@ function gameTimer() {
 
     }, 1000);
 }
-
-
-
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
